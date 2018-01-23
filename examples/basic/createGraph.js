2e5533905b969e9f60c1b31e6c628292f1d587dd
Mike Kaufman <mkaufman@microsoft.com>
2018-01-23 08:58:20 -0800

    checking in updates to example

--- examples/basic/createGraph.js
+++ examples/basic/createGraph.js
@@ -16,6 +16,11 @@ function getLinkNodeID(id) {
     return `Link ${id}`;
 }
 
+function getOutputNodeID(id) {
+    return `Output ${id}`;
+}
+
+/*
 var tempRecords = [{
         event: "executeBegin",
         executeID: 0
@@ -35,7 +40,6 @@ var tempRecords = [{
         event: "executeEnd",
         executeID: 0
     },
-
     {
         event: "executeBegin",
         executeID: 3,
@@ -107,12 +111,207 @@ var tempRecords = [{
         executeID: 12
     }
 ];
+*/
+
+var tempRecords = [{
+        event: "executeBegin",
+        executeID: 1
+    },
+    {
+        "event": "link",
+        "executeID": 1,
+        "linkID": 4,
+        "data": {
+            "type": "PROMISE"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 1,
+        "linkID": 4,
+        "causeID": 1
+    },
+    {
+        "event": "link",
+        "executeID": 1,
+        "linkID": 5,
+        "data": {
+            "type": "PROMISE"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 1,
+        "linkID": 5,
+        "causeID": 1
+    },
+    {
+        "event": "link",
+        "executeID": 1,
+        "linkID": 6,
+        "data": {
+            "type": "PROMISE"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 1,
+        "linkID": 6,
+        "causeID": 5
+    },
+    {
+        "event": "link",
+        "executeID": 1,
+        "linkID": 7,
+        "data": {
+            "type": "PROMISE"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 1,
+        "linkID": 7,
+        "causeID": 4
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 1
+    },
+
+    {
+        "event": "executeBegin",
+        "executeID": 6,
+        "causeID": 5
+    },
+    {
+        "event": "output",
+        "data": {
+            "output": "hello"
+        }
+    },
+    {
+        "event": "link",
+        "executeID": 6,
+        "linkID": 8,
+        "data": {
+            "type": "TickObject"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 6,
+        "linkID": 8,
+        "causeID": 6
+    },
+    {
+        "event": "link",
+        "executeID": 6,
+        "linkID": 9,
+        "data": {
+            "type": "Timeout"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 6,
+        "linkID": 9,
+        "causeID": 6
+    },
+    {
+        "event": "link",
+        "executeID": 6,
+        "linkID": 10,
+        "data": {
+            "type": "TIMERWRAP"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 6,
+        "linkID": 10,
+        "causeID": 6
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 6
+    },
+    {
+        "event": "executeBegin",
+        "executeID": 8,
+        "causeID": 6
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 8
+    },
+    {
+        "event": "executeBegin",
+        "executeID": 10,
+        "causeID": 6
+    },
+    {
+        "event": "executeBegin",
+        "executeID": 9,
+        "causeID": 6
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 9
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 10
+    },
+    {
+        "event": "executeBegin",
+        "executeID": 7,
+        "causeID": 4
+    },
+    {
+        "event": "output",
+        "data": {
+            "output": "in then: resolving promise in timeout"
+        }
+    },
+    {
+        "event": "link",
+        "executeID": 7,
+        "linkID": 11,
+        "data": {
+            "type": "TickObject"
+        }
+    },
+    {
+        "event": "cause",
+        "executeID": 7,
+        "linkID": 11,
+        "causeID": 7
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 7
+    },
+    {
+        "event": "executeBegin",
+        "executeID": 11,
+        "causeID": 7
+    },
+    {
+        "event": "executeEnd",
+        "executeID": 11
+    },
+
+];
 
 var nodes = {};
 
-function addNode(g, value) {
+function addNode(g, value, extra) {
     //jsav.displayInit();
-    const n = g.addNode(value);
+    let displayString = value;
+    if (extra) {
+        displayString = `${value} (${extra})`;
+    }
+    const n = g.addNode(displayString);
     nodes[value] = n;
     // g.layout();
     // jsav.step();
@@ -133,16 +332,19 @@ function addEdge(g, v1, v2) {
 
 function processNodes(records, g) {
 
+    let currentExecutionID = -1;
     for (let i = 0; i < records.length; i++) {
         const r = records[i];
         switch (r.event) {
             case 'executeBegin':
+                currentExecutionID = r.executeID;
                 addNode(g, getExecuteNodeID(r.executeID));
                 if (r.executeID != 0) {
                     addEdge(g, getCauseNodeID(r.causeID), getExecuteNodeID(r.executeID));
                 }
                 break;
             case 'executeEnd':
+                currentExecutionID = -1;
                 // change state on 'execute' node to "completed"
                 // event:
                 //  { type: 'executeEnd'
@@ -150,9 +352,13 @@ function processNodes(records, g) {
                 //   }
                 break;
             case 'link':
-
+{
                 // add link node
-                addNode(g, getLinkNodeID(r.linkID));
+                let extraData;
+                if (r.data && r.data.type) {
+                    extraData = r.data.type;
+                }
+                addNode(g, getLinkNodeID(r.linkID), extraData);
                 // add edge from current execution node to new link node
                 addEdge(g, getExecuteNodeID(r.executeID), getLinkNodeID(r.linkID));
 
@@ -162,6 +368,7 @@ function processNodes(records, g) {
                 //    linkID:  5,  // ID of new link node
                 //      executeID: n, // ID of current execute node
                 //   }
+            }
                 break;
             case 'cause':
                 // create "cause" node
@@ -177,7 +384,17 @@ function processNodes(records, g) {
                 // linkID:  q,
                 // executionID
                 //}
-
+                break;
+            case 'output':
+            {
+                let extraData;
+                if (r.data && r.data.output) {
+                    extraData = r.data.output;
+                }
+                addNode(g, getOutputNodeID(currentExecutionID), extraData);
+                // add edge from current execution node to new link node
+                addEdge(g, getExecuteNodeID(currentExecutionID), getOutputNodeID(currentExecutionID));
+            }
                 break;
             default:
                 console.error(`unexpected event value: ${r.event}`);
@@ -195,7 +412,7 @@ function initGraph(opts) {
     return g;
 };
 
-function populateGraph(g){
+function populateGraph(g) {
     processNodes(tempRecords, g);
     // var a = g.addNode("A"),
     //     b = g.addNode("B"),
@@ -211,58 +428,58 @@ function populateGraph(g){
     // g.addEdge(d, f);
 }
 
-    var l2 = jsav.label("Default automatic graph layout")
-    var g2 = initGraph({
-        layout: "layered"
-    });
+var l2 = jsav.label("Default automatic graph layout")
+var g2 = initGraph({
+    layout: "layered"
+});
 
-    //jsav.label("Layered graph layout")
-    // var g = initGraph({
-    //     layout: "layered"
-    // });
-    // g.layout();
-    jsav.displayInit();
-    populateGraph(g2);
-    //g2.layout();
-    //jsav.displayInit();
+//jsav.label("Layered graph layout")
+// var g = initGraph({
+//     layout: "layered"
+// });
+// g.layout();
+jsav.displayInit();
+populateGraph(g2);
+//g2.layout();
+//jsav.displayInit();
 
-    // hide the "other" graph
-    //g2.hide();
-    //l2.hide();
-    //jsav.step();
+// hide the "other" graph
+//g2.hide();
+//l2.hide();
+//jsav.step();
 
-    var preVisit = function (node, prev) {
-        node.addClass("processing");
-        jsav.umsg("Add node " + node.value() + " to the DFS search tree");
-        if (prev) {
-            node.edgeFrom(prev).css("stroke", "red"); // highlight
-            node.edgeTo(prev).css("stroke", "red"); // highlight
+var preVisit = function (node, prev) {
+    node.addClass("processing");
+    jsav.umsg("Add node " + node.value() + " to the DFS search tree");
+    if (prev) {
+        node.edgeFrom(prev).css("stroke", "red"); // highlight
+        node.edgeTo(prev).css("stroke", "red"); // highlight
+    }
+    jsav.step();
+};
+var visit = function (node) {
+    node.addClass("visited");
+    jsav.umsg("Call DFS for node " + node.value());
+    jsav.step();
+};
+var postVisit = function (node) {
+    node.addClass("finished");
+    jsav.umsg("Done with node " + node.value());
+    jsav.step();
+};
+var dfs = function (start, prev) {
+    var successors,
+        next;
+    preVisit(start, prev);
+    successors = start.neighbors();
+    for (next = successors.next(); next; next = successors.next()) {
+        if (!next.hasClass("visited")) {
+            visit(next);
+            dfs(next, start);
         }
-        jsav.step();
-    };
-    var visit = function (node) {
-        node.addClass("visited");
-        jsav.umsg("Call DFS for node " + node.value());
-        jsav.step();
-    };
-    var postVisit = function (node) {
-        node.addClass("finished");
-        jsav.umsg("Done with node " + node.value());
-        jsav.step();
-    };
-    var dfs = function (start, prev) {
-        var successors,
-            next;
-        preVisit(start, prev);
-        successors = start.neighbors();
-        for (next = successors.next(); next; next = successors.next()) {
-            if (!next.hasClass("visited")) {
-                visit(next);
-                dfs(next, start);
-            }
-        }
-        postVisit(start);
-    };
-    //visit(g.nodes()[0]);
-    //dfs(g.nodes()[0]);
-    jsav.recorded();
+    }
+    postVisit(start);
+};
+//visit(g.nodes()[0]);
+//dfs(g.nodes()[0]);
+jsav.recorded();
