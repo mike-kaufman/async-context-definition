73c5cb3c87c22c4db218197df98d0ba48a5a82e6
Mike Kaufman <mkaufman@microsoft.com>
2018-01-25 14:33:03 -0800

    updated styling of graph

--- examples/lib/slideShow/createGraph.js
+++ examples/lib/slideShow/createGraph.js
@@ -13,7 +13,7 @@ function getLinkNodeID(id) {
 }
 
 function getOutputNodeID(id) {
-    return `Output ${id}`;
+    return `Output ${id}:`;
 }
 
 var nodes = {};
@@ -25,16 +25,22 @@ function addNode(g, value, extra) {
     }
     const n = g.addNode(displayString);
     nodes[value] = n;
- }
+    return n;
+}
 
 function getNode(value) {
     return nodes[value];
 }
 
-function addEdge(g, v1, v2) {
+// see valid opts for Edge:
+//  -  http://jsav.io/graphicalprimitives/lines/
+//  -  https://dmitrybaranovskiy.github.io/raphael/reference.html
+function addEdge(g, v1, v2, opts) {
     const n1 = getNode(v1);
     const n2 = getNode(v2);
-    g.addEdge(n1, n2);
+    opts = opts || {};
+    opts['arrow-end'] = 'classic-wide-long'; /*classic, block, open, oval, diamond, none,*/;
+    return g.addEdge(n1, n2, opts);
 }
 
 const jsavCode = jsav.code(codeLines);
@@ -46,62 +52,65 @@ function processNodes(records, g) {
     for (let i = 0; i < records.length; i++) {
         const r = records[i];
 
-         // handle code highlighting
-         if (r.data && r.data.highlightLine !== undefined) {
+        // handle code highlighting
+        if (r.data && r.data.highlightLine !== undefined) {
             if (currentHighlightLine > 0) {
                 jsavCode.unhighlight(currentHighlightLine);
             }
             jsavCode.highlight(r.data.highlightLine);
             currentHighlightLine = r.data.highlightLine;
-         }
+        }
 
         switch (r.event) {
             case 'executeBegin':
                 currentExecutionID = r.executeID;
                 addNode(g, getExecuteNodeID(r.executeID));
+                //getNode(getExecuteNodeID(r.executeID)).css('background-color', '#3ade5b');
+                getNode(getExecuteNodeID(r.executeID)).addClass('activeExecutionNode');
                 if (r.executeID != 0) {
                     addEdge(g, getCauseNodeID(r.causeID), getExecuteNodeID(r.executeID));
                 }
                 break;
             case 'executeEnd':
+                if (currentHighlightLine > 0) {
+                    jsavCode.unhighlight(currentHighlightLine);
+                    currentHighlightLine = -1;
+                }
                 currentExecutionID = -1;
-                // change state on 'execute' node to "completed"
-                // event:
-                //  { type: 'executeEnd'
-                //      executeID:  
-                //   }
+                //getNode(getExecuteNodeID(r.executeID)).css('background-color', '##cc0000');
+                getNode(getExecuteNodeID(r.executeID)).removeClass('activeExecutionNode').addClass('completedExecutionNode');
                 break;
             case 'link':
-{
-                // add link node
-                let extraData;
-                if (r.data && r.data.type) {
-                    extraData = r.data.type;
-                }
-                addNode(g, getLinkNodeID(r.linkID), extraData);
-                // add edge from current execution node to new link node
-                addEdge(g, getExecuteNodeID(r.executeID), getLinkNodeID(r.linkID));
+                {
+                    // add link node
+                    let extraData;
+                    if (r.data && r.data.type) {
+                        extraData = r.data.type;
+                    }
+                    addNode(g, getLinkNodeID(r.linkID), extraData);
+                    // add edge from current execution node to new link node
+                    addEdge(g, getExecuteNodeID(r.executeID), getLinkNodeID(r.linkID));
 
-            }
+                }
                 break;
             case 'cause':
                 // create "cause" node
-                addNode(g, getCauseNodeID(r.causeID));
+                addNode(g, getCauseNodeID(r.causeID)).addClass('causeNode');
                 // create "edge to current execution edge"
-                addEdge(g, getExecuteNodeID(r.executeID), getCauseNodeID(r.causeID))
+                addEdge(g, getExecuteNodeID(r.executeID), getCauseNodeID(r.causeID), { 'stroke-dasharray': ['- .'], 'arrow-end': 'block'/*, classic, block, open, oval, diamond, none,*/ });
                 // create "linked-by" edge
                 addEdge(g, getLinkNodeID(r.linkID), getCauseNodeID(r.causeID));
                 break;
             case 'output':
-            {
-                let extraData;
-                if (r.data && r.data.output) {
-                    extraData = r.data.output;
+                {
+                    let extraData;
+                    if (r.data && r.data.output) {
+                        extraData = r.data.output;
+                    }
+                    addNode(g, getOutputNodeID(currentExecutionID), extraData).addClass('outputNode');
+                    // add edge from current execution node to new link node
+                    addEdge(g, getExecuteNodeID(currentExecutionID), getOutputNodeID(currentExecutionID));
                 }
-                addNode(g, getOutputNodeID(currentExecutionID), extraData);
-                // add edge from current execution node to new link node
-                addEdge(g, getExecuteNodeID(currentExecutionID), getOutputNodeID(currentExecutionID));
-            }
                 break;
             default:
                 console.error(`unexpected event value: ${r.event}`);
